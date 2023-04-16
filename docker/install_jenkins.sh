1fa8ae4099896d5f8702f4ffd595d6eb45389290
JAVIER SANCHEZ PAEZ <javier.sanchez.paez@kyndryl.com>
2023-04-16 11:59:46 +0200

    Nueva estructura

---
+++ docker/install_jenkins.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+apt-get update
+apt-get install curl -y
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+apt-get update
+apt-get install openjdk-11-jre jenkins -y
